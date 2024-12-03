a3ca587b65655ea25a9fbad54f5efe967e26fb1d
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-12-03 02:35:59 -0300

    Upload de segunda

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -5,34 +5,50 @@ import { MoonStar } from '~/lib/icons/MoonStar';
 import { Sun } from '~/lib/icons/Sun';
 import { useColorScheme } from '~/lib/useColorScheme';
 import { cn } from '~/lib/utils';
+import { Avatar, AvatarFallback } from '~/components/ui/avatar';
+import { Text } from './ui/text';
 
 export function ThemeToggle() {
   const { isDarkColorScheme, setColorScheme } = useColorScheme();
+
+  const toggleTheme = () => {
+    const newTheme = isDarkColorScheme ? 'light' : 'dark';
+    setColorScheme(newTheme);
+    setAndroidNavigationBar(newTheme);
+    AsyncStorage.setItem('theme', newTheme);
+  };
+
   return (
-    <Pressable
-      onPress={() => {
-        const newTheme = isDarkColorScheme ? 'light' : 'dark';
-        setColorScheme(newTheme);
-        setAndroidNavigationBar(newTheme);
-        AsyncStorage.setItem('theme', newTheme);
-      }}
+    <View
       className='
+        flex flex-row aspect-square pt-0.5 justify-center items-start px-20 gap-x-8
         web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2'
     >
-      {({ pressed }) => (
-        <View
-          className={cn(
-            'flex-1 aspect-square pt-0.5 justify-center items-start px-1',
-            pressed && 'opacity-70'
-          )}
-        >
-          {isDarkColorScheme ? (
-            <Sun className='text-foreground' size={24} strokeWidth={1.25} />
+      <Pressable
+        onPress={toggleTheme}
+        className='mt-1 mx-5'
+      >
+        {({ pressed }) =>
+          isDarkColorScheme ? (
+            <Sun
+              className={cn('text-foreground', pressed && 'opacity-70')}
+              size={30}
+              strokeWidth={1.25}
+            />
           ) : (
-            <MoonStar className='text-foreground' size={23} strokeWidth={1.25} />
-          )}
-        </View>
-      )}
-    </Pressable>
+            <MoonStar
+              className={cn('text-foreground', pressed && 'opacity-70')}
+              size={30}
+              strokeWidth={1.25}
+            />
+          )
+        }
+      </Pressable>
+      <Avatar className='w-10 h-10' alt="Utilizador">
+        <AvatarFallback className='bg-muted-foreground'>
+          <Text className='font-regular text-secondary'>JC</Text>
+        </AvatarFallback>
+      </Avatar>
+    </View>
   );
 }
